8362cefa6b94dcb993121ae0808dedc9dc542fff
Abu Nasim <abu_nasim1033@uits.edu.bd>
2026-04-02 17:54:51 +0600

    Add files via upload

---
+++ Part_1_fundamentals/first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hello from "./Hello";
+
+const App = () => {
+  return (
+    <>
+      <h1>
+        This is out First Component
+        <Hello />
+      </h1>
+      <p>Nasim</p>
+    </>
+  );
+};
+export default App;
